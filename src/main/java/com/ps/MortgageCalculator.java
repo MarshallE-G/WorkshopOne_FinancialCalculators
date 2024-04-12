53b7850bbc36b0a4f576b1226cd0a3fe3224d81a
Marshall <mellisgibbs@gmail.com>
2024-04-12 14:11:38 -0400

    Finished pseudocode and created static method

--- src/main/java/com/ps/MortgageCalculator.java
+++ src/main/java/com/ps/MortgageCalculator.java
@@ -2,4 +2,16 @@ package com.ps;
 
 public class MortgageCalculator extends Main {
 
+    // Create static method
+        // Find out principal amount from user
+        // Find interest rate from user
+        // Find loan length/duration from user
+        // Create compounding interest formula for monthly loan amount
+        // Find interest amount for loan duration
+        // Return a sentence stating monthly loan amount and interest for loan duration
+    public static float mortgageCalculator(float principal, float interestRate, float loanDuration) {
+
+        return 0;
+    }
+
 }
