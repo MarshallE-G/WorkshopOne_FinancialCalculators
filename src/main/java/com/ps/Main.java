46fe29898186cdd615daceac3aabad03abbb45d9
Marshall <mellisgibbs@gmail.com>
2024-04-12 15:41:46 -0400

    Changed interest rate input from user into decimal

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,10 +1,27 @@
 package com.ps;
 import java.util.Scanner;
 
+import static com.ps.MortgageCalculator.mortgageCalculator;
+
 public class Main {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        float principal = 0;
+        float interestRate = 0;
+        int loanDuration = 0;
+
+        System.out.print("What is your Principal amount? ");
+        principal = scanner.nextFloat();
+        System.out.println();
+        System.out.print("What is the monthly interest rate? ");
+        interestRate = scanner.nextFloat();
+        System.out.println();
+        System.out.print("What is the loan length? ");
+        loanDuration = scanner.nextInt();
+        System.out.println();
+
+        mortgageCalculator(principal, interestRate, loanDuration);
+        scanner.close();
     }
 }
--- src/main/java/com/ps/MortgageCalculator.java
+++ src/main/java/com/ps/MortgageCalculator.java
@@ -10,23 +10,14 @@ public class MortgageCalculator extends Main {
         // Find interest amount for loan duration
         // Return a sentence stating monthly loan amount and interest for loan duration
     public static String mortgageCalculator(float principal, float interestRate, int loanDuration) {
-        System.out.print("What is your Principal amount? ");
-        principal = scanner.nextFloat();
-        System.out.println();
-        System.out.print("What is the monthly interest rate? ");
-        interestRate = scanner.nextFloat();
-        System.out.println();
-        System.out.print("What is the loan length? ");
-        loanDuration = scanner.nextInt();
-        System.out.println();
-
-        float monthlyLoanAmount = (float) (principal * Math.pow((1 + (interestRate/12)), (12*loanDuration)));
+        float interestRateInDecimals = interestRate/100;
+        float monthlyLoanAmount = (float) (principal * Math.pow((1 + (interestRateInDecimals/12)), (12*loanDuration)));
         float totalInterest = (monthlyLoanAmount * (12*loanDuration)) - principal;
         /* A $53,000 loan at 7.625% interest for 15 years would have a $495.09/mo payment with a total interest
          * of $36,115.99
          */
         String theExpectedMonthlyPaymentAndTotalInterestPaid =
-                "A $" + String.format("%.2f", principal) + " loan at " + String.format("%.2f", interestRate)
+                "A $" + String.format("%.2f", principal) + " loan at " + String.format("%.2f", interestRateInDecimals)
                         + "% interest for " + loanDuration + " years would have a $"
                         + String.format("%.2f", monthlyLoanAmount) + "/mo payment with a total interest of $"
                         + String.format("%.2f", totalInterest) + ".\n";
