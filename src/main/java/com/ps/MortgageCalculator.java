6ee5c8d78e801e45b978b4da7d6ab713cda4a3e5
Marshall <mellisgibbs@gmail.com>
2024-04-12 15:32:09 -0400

    Completed the Mortgage Calculator. Changed return type to String.

--- src/main/java/com/ps/MortgageCalculator.java
+++ src/main/java/com/ps/MortgageCalculator.java
@@ -9,7 +9,7 @@ public class MortgageCalculator extends Main {
         // Create compounding interest formula for monthly loan amount
         // Find interest amount for loan duration
         // Return a sentence stating monthly loan amount and interest for loan duration
-    public static float mortgageCalculator(float principal, float interestRate, float loanDuration) {
+    public static String mortgageCalculator(float principal, float interestRate, int loanDuration) {
         System.out.print("What is your Principal amount? ");
         principal = scanner.nextFloat();
         System.out.println();
@@ -17,13 +17,21 @@ public class MortgageCalculator extends Main {
         interestRate = scanner.nextFloat();
         System.out.println();
         System.out.print("What is the loan length? ");
-        loanDuration = scanner.nextFloat();
+        loanDuration = scanner.nextInt();
         System.out.println();
 
-        float monthlyLoanAmount = (principal * (interestRate/12)) * Math.pow((1 + (loanDuration)), );
+        float monthlyLoanAmount = (float) (principal * Math.pow((1 + (interestRate/12)), (12*loanDuration)));
+        float totalInterest = (monthlyLoanAmount * (12*loanDuration)) - principal;
+        /* A $53,000 loan at 7.625% interest for 15 years would have a $495.09/mo payment with a total interest
+         * of $36,115.99
+         */
+        String theExpectedMonthlyPaymentAndTotalInterestPaid =
+                "A $" + String.format("%.2f", principal) + " loan at " + String.format("%.2f", interestRate)
+                        + "% interest for " + loanDuration + " years would have a $"
+                        + String.format("%.2f", monthlyLoanAmount) + "/mo payment with a total interest of $"
+                        + String.format("%.2f", totalInterest) + ".\n";
 
-
-        return 0;
+        return theExpectedMonthlyPaymentAndTotalInterestPaid;
     }
 
 }
