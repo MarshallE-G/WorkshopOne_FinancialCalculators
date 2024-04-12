7811598bbb5f72c4ca16cf335ed9e8105cd4c3e3
Marshall <mellisgibbs@gmail.com>
2024-04-12 14:37:15 -0400

    Finished asking user for input and scanning user repsonse

--- src/main/java/com/ps/MortgageCalculator.java
+++ src/main/java/com/ps/MortgageCalculator.java
@@ -10,6 +10,18 @@ public class MortgageCalculator extends Main {
         // Find interest amount for loan duration
         // Return a sentence stating monthly loan amount and interest for loan duration
     public static float mortgageCalculator(float principal, float interestRate, float loanDuration) {
+        System.out.print("What is your Principal amount? ");
+        principal = scanner.nextFloat();
+        System.out.println();
+        System.out.print("What is the monthly interest rate? ");
+        interestRate = scanner.nextFloat();
+        System.out.println();
+        System.out.print("What is the loan length? ");
+        loanDuration = scanner.nextFloat();
+        System.out.println();
+
+        float monthlyLoanAmount = (principal * (interestRate/12)) * Math.pow((1 + (loanDuration)), );
+
 
         return 0;
     }
