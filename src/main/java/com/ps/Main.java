45a9b43dbc1be53140ea87a21e5f8173d9be0a91
Marshall <mellisgibbs@gmail.com>
2024-04-13 22:49:32 -0400

    Asked user to select a calculator. Moved questions for calculators and result to switch statement.

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -11,6 +11,46 @@ public class Main {
         int loanDuration = 0; // From Mortgage Calculator
         int numOfYears = 0; // From Future Value Calculator
 
+        System.out.println(
+                "Which calculator would you like to you use?\n" +
+                        "If Mortgage Calculator, type 1\n" +
+                        "If Future Value Calculator, type 2"
+        );
+        System.out.println("Enter here: ");
+        String selection = scanner.nextLine();
+
+        switch (selection) {
+            case "1":
+                System.out.print("What is your Principal amount? ");
+                principal = scanner.nextFloat();
+                System.out.println();
+                System.out.print("What is the monthly interest rate in %? ");
+                interestRate = scanner.nextFloat();
+                System.out.println();
+                System.out.print("What is the loan length in years? ");
+                loanDuration = scanner.nextInt();
+                System.out.println();
+
+                System.out.println(Calculators.mortgageCalculator(principal, interestRate, loanDuration));
+                break;
+            case "2":
+                System.out.print("What is your CD's deposit amount? ");
+                principal = scanner.nextFloat();
+                System.out.println();
+                System.out.print("What is the interest rate in %? ");
+                interestRate = scanner.nextFloat();
+                System.out.println();
+                System.out.print("How many years will it take for your CD to mature? ");
+                numOfYears = scanner.nextInt();
+                System.out.println();
+
+                System.out.println(Calculators.fVCalculator(principal, interestRate, numOfYears));
+                break;
+            default:
+                System.out.println("ERROR: Type either 1 or 2.");
+                break;
+        }
+
         /*System.out.print("What is your Principal amount? ");
         principal = scanner.nextFloat();
         System.out.println();
@@ -19,11 +59,11 @@ public class Main {
         System.out.println();
         System.out.print("What is the loan length in years? ");
         loanDuration = scanner.nextInt();
-        System.out.println();*/
+        System.out.println();
 
-        // System.out.println(Calculators.mortgageCalculator(principal, interestRate, loanDuration));
+        System.out.println(Calculators.mortgageCalculator(principal, interestRate, loanDuration));*/
 
-        System.out.print("What is your CD's deposit amount? ");
+        /*System.out.print("What is your CD's deposit amount? ");
         principal = scanner.nextFloat();
         System.out.println();
         System.out.print("What is the interest rate in %? ");
@@ -33,7 +73,7 @@ public class Main {
         numOfYears = scanner.nextInt();
         System.out.println();
 
-        System.out.println(Calculators.fVCalculator(principal, interestRate, numOfYears));
+        System.out.println(Calculators.fVCalculator(principal, interestRate, numOfYears));*/
 
         scanner.close();
     }
