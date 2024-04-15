0728e1708caebf3071de197abd5e625a7dbf8707
Marshall <mellisgibbs@gmail.com>
2024-04-15 15:37:41 -0400

    Moved variables, user interface, and the calling of each calculator method FROM switch statement and into individual static methods. Switch statement now calls those static methods.

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -5,13 +5,6 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        float principal = 0; // Used in Mortgage and Future Value Calculator
-        float interestRate = 0; // Used in all calculator methods
-        float monthlyPayout = 0; // Used in Present Value Calculator
-
-        int loanDuration = 0; // Used in Mortgage Calculator
-        int numOfYears = 0; // Used in Future Value Calculator and Present Value Calculator
-
         System.out.println(
                 "Which calculator would you like to use?\n\n" +
                         "If Mortgage Calculator, type 1\n" +
@@ -25,48 +18,15 @@ public class Main {
         switch (selection) {
             // Mortgage Calculator
             case "1":
-                System.out.println("You selected the 'Mortgage Calculator'");
-                System.out.print("What is your Principal amount? ");
-                principal = scanner.nextFloat();
-                System.out.println();
-                System.out.print("What is the monthly interest rate in %? ");
-                interestRate = scanner.nextFloat();
-                System.out.println();
-                System.out.print("What is the loan length in years? ");
-                loanDuration = scanner.nextInt();
-                System.out.println();
-
-                System.out.println(Calculators.mortgageCalculator(principal, interestRate, loanDuration));
+                ifMortgage();
                 break;
             // Future Value Calculator
             case "2":
-                System.out.println("You selected the 'Future Value Calculator'");
-                System.out.print("What is your CD's deposit amount? ");
-                principal = scanner.nextFloat();
-                System.out.println();
-                System.out.print("What is the interest rate in %? ");
-                interestRate = scanner.nextFloat();
-                System.out.println();
-                System.out.print("How many years will it take for your CD to mature? ");
-                numOfYears = scanner.nextInt();
-                System.out.println();
-
-                System.out.println(Calculators.fVCalculator(principal, interestRate, numOfYears));
+                ifFV();
                 break;
             // Present Value Calculator
             case "3":
-                System.out.println("You selected the 'Present Value Calculator'");
-                System.out.print("What is your monthly payout amount? ");
-                monthlyPayout = scanner.nextFloat();
-                System.out.println();
-                System.out.print("What is the interest rate in %? ");
-                interestRate = scanner.nextFloat();
-                System.out.println();
-                System.out.print("How many years will you have to pay out? ");
-                numOfYears = scanner.nextInt();
-                System.out.println();
-
-                System.out.println(Calculators.pVCalculator(monthlyPayout, interestRate, numOfYears));
+                ifPV();
                 break;
             default:
                 System.out.println("ERROR: Type either 1, 2, or 3.");
@@ -74,5 +34,65 @@ public class Main {
         }
 
         scanner.close();
+    }
+
+    // Generates user interface, runs mortgage calculator, and prints mortgage calculator statement.
+    public static void ifMortgage() {
+        float principal = 0;
+        float interestRate = 0;
+        int loanDuration = 0;
+
+        System.out.println("You selected the 'Mortgage Calculator'");
+        System.out.print("What is your Principal amount? ");
+        principal = scanner.nextFloat();
+        System.out.println();
+        System.out.print("What is the monthly interest rate in %? ");
+        interestRate = scanner.nextFloat();
+        System.out.println();
+        System.out.print("What is the loan length in years? ");
+        loanDuration = scanner.nextInt();
+        System.out.println();
+
+        System.out.println(Calculators.mortgageCalculator(principal, interestRate, loanDuration));
+    }
+
+    // Generates user interface, runs future value calculator, and prints future value calculator statement.
+    public static void ifFV() {
+        float principal = 0;
+        float interestRate = 0;
+        int numOfYears = 0;
+
+        System.out.println("You selected the 'Future Value Calculator'");
+        System.out.print("What is your CD's deposit amount? ");
+        principal = scanner.nextFloat();
+        System.out.println();
+        System.out.print("What is the interest rate in %? ");
+        interestRate = scanner.nextFloat();
+        System.out.println();
+        System.out.print("How many years will it take for your CD to mature? ");
+        numOfYears = scanner.nextInt();
+        System.out.println();
+
+        System.out.println(Calculators.fVCalculator(principal, interestRate, numOfYears));
+    }
+
+    // Generates user interface, runs present value calculator, and prints present value calculator statement.
+    public static void ifPV() {
+        float monthlyPayout = 0;
+        float interestRate = 0;
+        int numOfYears = 0;
+
+        System.out.println("You selected the 'Present Value Calculator'");
+        System.out.print("What is your monthly payout amount? ");
+        monthlyPayout = scanner.nextFloat();
+        System.out.println();
+        System.out.print("What is the interest rate in %? ");
+        interestRate = scanner.nextFloat();
+        System.out.println();
+        System.out.print("How many years will you have to pay out? ");
+        numOfYears = scanner.nextInt();
+        System.out.println();
+
+        System.out.println(Calculators.pVCalculator(monthlyPayout, interestRate, numOfYears));
     }
 }
